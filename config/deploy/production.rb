a8c2bdec3b3f493a65e754189583a096f43a4137
aristotelesbr <aristotelesbr2014@gmail.com>
2016-06-11 21:16:41 +0000

    fix cap deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 server '192.168.0.12', user: 'deploy', roles: %w{web app db}
-
+set :rails_env, :production
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
